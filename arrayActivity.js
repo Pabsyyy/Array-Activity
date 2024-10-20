51ee86fde93b41c4c1e5e51bdd629356f3669a2a
Pabsyyy <christiangalinato1218@gmail.com>
2024-10-20 10:44:40 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+// Favorite Singers
+let favSingers = ["Drake", "Daniel Caesar", "Brent Faiyaz"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["12", "18", "01", "07"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
